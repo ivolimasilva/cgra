b3c03cfaac9c7e6443ac65c3a6259b97db076793
Guilherme Lawless <guilawless@live.com>
2015-05-17 19:14:44 +0100

    'H' option for waving

--- tp6/MyPaperPlane.js
+++ tp6/MyPaperPlane.js
@@ -6,29 +6,29 @@
 var degToRad = Math.PI / 180.0;
 
 function MyPaperPlane(scene, speed, flyTo, rotateTo, fallTo) {
-    CGFobject.call(this, scene);
+	CGFobject.call(this, scene);
 
-    this.time2speed = speed / (60.0 * 1000.0);
-    this.rotateTime2speed = 50.0 / (60.0 * 1000.0);
-    this.fallTime2speed = 0.05 / (60.0 * 1000.0);
+	this.time2speed = speed / (60.0 * 1000.0);
+	this.rotateTime2speed = 50.0 / (60.0 * 1000.0);
+	this.fallTime2speed = 0.05 / (60.0 * 1000.0);
 
-    this.flyTo = flyTo;
-    this.rotateTo = rotateTo;
-    this.fallTo = fallTo;
+	this.flyTo = flyTo;
+	this.rotateTo = rotateTo;
+	this.fallTo = fallTo;
 
-    this.hasCrashed = 0;
-    this.hasFallen = 0;
+	this.hasCrashed = 0;
+	this.hasFallen = 0;
 
-    this.offsetX = 0;
-    this.offsetY = 0;
-    this.offsetZ = 0;
-    this.rotateX = 0;
-    this.rotateY = 0;
-    this.rotateZ = 0;
-    this.rotateAmount = [0, 0, 0];
+	this.offsetX = 0;
+	this.offsetY = 0;
+	this.offsetZ = 0;
+	this.rotateX = 0;
+	this.rotateY = 0;
+	this.rotateZ = 0;
+	this.rotateAmount = [0, 0, 0];
 
 
-    this.initBuffers();
+	this.initBuffers();
 
 };
 
@@ -37,108 +37,108 @@ MyPaperPlane.prototype.constructor = MyPaperPlane;
 
 MyPaperPlane.prototype.update = function (currTime) {
 
-    if (typeof this.timeBegin == 'undefined')
-        this.timeBegin = currTime;
+	if (typeof this.timeBegin == 'undefined')
+		this.timeBegin = currTime;
 
-    else if (this.hasFallen == 0) {
-        if (this.hasCrashed == 0) {
-            this.setOffset('x', this.flyTo[0] * this.time2speed * (currTime - this.timeBegin));
-            this.setOffset('y', this.flyTo[1] * this.time2speed * (currTime - this.timeBegin));
-            this.setOffset('z', (0.5 * Math.sin(0.0031415 * (currTime - this.timeBegin)) + this.flyTo[2]) * this.time2speed * (currTime - this.timeBegin));
+	else if (this.hasFallen == 0) {
+		if (this.hasCrashed == 0) {
+			this.setOffset('x', this.flyTo[0] * this.time2speed * (currTime - this.timeBegin));
+			this.setOffset('y', this.flyTo[1] * this.time2speed * (currTime - this.timeBegin));
+			this.setOffset('z', (0.5 * Math.sin(0.0031415 * (currTime - this.timeBegin)) + this.flyTo[2]) * this.time2speed * (currTime - this.timeBegin));
 
-            this.testCrash();
-        }
+			this.testCrash();
+		}
 
-        if (this.hasCrashed == 1) {
-            if (typeof this.timeCrash == 'undefined')
-                this.timeCrash = currTime;
+		if (this.hasCrashed == 1) {
+			if (typeof this.timeCrash == 'undefined')
+				this.timeCrash = currTime;
 
-            else if (this.hasFallen == 0) // movimento de queda
-            {
-                this.setOffset('x', this.flyTo[0] + (-3 * Math.sin(0.0031415 * (currTime - this.timeBegin)) + this.fallTo[0]) * this.fallTime2speed * Math.pow(currTime - this.timeCrash, 2));
-                this.setOffset('y', this.flyTo[1] + this.fallTo[1] * this.fallTime2speed * Math.pow(currTime - this.timeCrash, 2));
-                this.setOffset('z', this.flyTo[2] + this.fallTo[2] * this.fallTime2speed * Math.pow(currTime - this.timeCrash, 2));
-                this.setRotate('x', this.rotateTo[0] * this.rotateTime2speed * (currTime - this.timeCrash));
-                this.setRotate('y', this.rotateTo[1] * this.rotateTime2speed * (currTime - this.timeCrash));
-                this.setRotate('z', this.rotateTo[2] * this.rotateTime2speed * (currTime - this.timeCrash));
+			else if (this.hasFallen == 0) // movimento de queda
+			{
+				this.setOffset('x', this.flyTo[0] + (-3 * Math.sin(0.0031415 * (currTime - this.timeBegin)) + this.fallTo[0]) * this.fallTime2speed * Math.pow(currTime - this.timeCrash, 2));
+				this.setOffset('y', this.flyTo[1] + this.fallTo[1] * this.fallTime2speed * Math.pow(currTime - this.timeCrash, 2));
+				this.setOffset('z', this.flyTo[2] + this.fallTo[2] * this.fallTime2speed * Math.pow(currTime - this.timeCrash, 2));
+				this.setRotate('x', this.rotateTo[0] * this.rotateTime2speed * (currTime - this.timeCrash));
+				this.setRotate('y', this.rotateTo[1] * this.rotateTime2speed * (currTime - this.timeCrash));
+				this.setRotate('z', this.rotateTo[2] * this.rotateTime2speed * (currTime - this.timeCrash));
 
-                this.testFall();
-            }
-        }
-    }
+				this.testFall();
+			}
+		}
+	}
 }
 
 
 MyPaperPlane.prototype.testCrash = function () {
-    if (this.offsetY >= this.flyTo[1]) {
-        this.hasCrashed = 1;
-    }
+	if (this.offsetY >= this.flyTo[1]) {
+		this.hasCrashed = 1;
+	}
 }
 
 MyPaperPlane.prototype.testFall = function () {
-    if (this.offsetZ <= this.flyTo[2] + this.fallTo[2] - 0.5) {
-        this.hasFallen = 1;
-    }
+	if (this.offsetZ <= this.flyTo[2] + this.fallTo[2] - 0.5) {
+		this.hasFallen = 1;
+	}
 }
 
 
 MyPaperPlane.prototype.setOffset = function (coord, amount) {
-    switch (coord) {
-    case 'x':
-        this.offsetX = amount;
-        break;
-    case 'y':
-        this.offsetY = amount;
-        break;
-    case 'z':
-        this.offsetZ = amount;
-        break;
-    default:
-        console.log('MyPaperPlane.prototype.setOffset -- wrong coord!');
-    }
+	switch (coord) {
+	case 'x':
+		this.offsetX = amount;
+		break;
+	case 'y':
+		this.offsetY = amount;
+		break;
+	case 'z':
+		this.offsetZ = amount;
+		break;
+	default:
+		console.log('MyPaperPlane.prototype.setOffset -- wrong coord!');
+	}
 }
 
 MyPaperPlane.prototype.setRotate = function (coord, amount) {
-    switch (coord) {
-    case 'x':
-        this.rotateAmount[0] = amount;
-        break;
-    case 'y':
-        this.rotateAmount[1] = amount;
-        break;
-    case 'z':
-        this.rotateAmount[2] = amount;
-        break;
-    default:
-        console.log('MyPaperPlane.prototype.setRotate -- wrong coord!');
-    }
+	switch (coord) {
+	case 'x':
+		this.rotateAmount[0] = amount;
+		break;
+	case 'y':
+		this.rotateAmount[1] = amount;
+		break;
+	case 'z':
+		this.rotateAmount[2] = amount;
+		break;
+	default:
+		console.log('MyPaperPlane.prototype.setRotate -- wrong coord!');
+	}
 }
 
 
 MyPaperPlane.prototype.display = function () {
-    this.scene.translate(this.offsetX, this.offsetY, this.offsetZ);
-    this.scene.rotate(this.rotateAmount[2], 0, 0, 1);
-    this.scene.rotate(this.rotateAmount[1], 0, 1, 0);
-    this.scene.rotate(this.rotateAmount[0], 1, 0, 0);
+	this.scene.translate(this.offsetX, this.offsetY, this.offsetZ);
+	this.scene.rotate(this.rotateAmount[2], 0, 0, 1);
+	this.scene.rotate(this.rotateAmount[1], 0, 1, 0);
+	this.scene.rotate(this.rotateAmount[0], 1, 0, 0);
 
-    CGFobject.prototype.display.call(this);
+	CGFobject.prototype.display.call(this);
 }
 
 
 MyPaperPlane.prototype.initBuffers = function () {
 
-    /*
-     *  y^
-     *0.5|      0
-     *   |
-     *0  |
-     *   |
-     *-.5|1    2/4    3
-     *   -------------> x
-     *   -0.5   0   0.5
-     */
+	/*
+	 *  y^
+	 *0.5|      0
+	 *   |
+	 *0  |
+	 *   |
+	 *-.5|1    2/4    3
+	 *   -------------> x
+	 *   -0.5   0   0.5
+	 */
 
-    this.vertices = [
+	this.vertices = [
   0, 0.5, 0,
   -0.5, -0.5, 0,
   0, -0.5, 0,
@@ -146,7 +146,7 @@ MyPaperPlane.prototype.initBuffers = function () {
   0, -0.5, -0.2
   ];
 
-    this.indices = [
+	this.indices = [
   /*top view*/
   0, 1, 2,
   0, 2, 3,
@@ -160,7 +160,7 @@ MyPaperPlane.prototype.initBuffers = function () {
   ];
 
 
-    this.normals = [
+	this.normals = [
      0, 0, 1,
      0, 0, 1,
      0, 0, 1,
@@ -169,6 +169,6 @@ MyPaperPlane.prototype.initBuffers = function () {
     ];
 
 
-    this.primitiveType = this.scene.gl.TRIANGLES;
-    this.initGLBuffers();
+	this.primitiveType = this.scene.gl.TRIANGLES;
+	this.initGLBuffers();
 };
